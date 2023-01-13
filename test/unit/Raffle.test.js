446668ec168d4e6c1d90c0115989151edc334c52
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-12 19:07:44 -0700

    added interval function

---
+++ test/unit/Raffle.test.js
@@ -0,0 +1,25 @@
+const { assert } = require("chai")
+const { getNamedAccounts, deployments, ethers } = require("hardhat")
+const {developmentChains} = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name) 
+    ? describe.skip 
+    : describe("Raffle Unit Tests", async function () {
+        let raffle, VRFCoordinatorV2Mock
+
+        beforeEach(async function (){
+            const {deployer} = await getNamedAccounts()
+            await deployments.fixture(["all"])
+            raffle = await ethers.getContract("Raffle", deployer)
+            VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+        })
+
+        describe("constructor", async function(){
+            it("initializes the raffle correctly", async function(){
+                // Ideally we make our test have just 1 assert per it()
+                const raffleState = await raffle.getRaffleState()
+                const interval = await interval.getInterval
+                assert.equal(raffleState.toString(), "0")
+            })
+        })
+    })
