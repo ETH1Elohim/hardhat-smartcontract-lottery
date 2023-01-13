a23097a2625ea64cceacc45a75ff634c0dd848a6
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-13 11:34:25 -0700

    revert & record tests

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,27 +1,43 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { getNamedAccounts, deployments, ethers } = require("hardhat")
-const {developmentChains, networkConfig} = require("../../helper-hardhat-config")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
-!developmentChains.includes(network.name) 
-    ? describe.skip 
+!developmentChains.includes(network.name)
+    ? describe.skip
     : describe("Raffle Unit Tests", async function () {
-        let raffle, VRFCoordinatorV2Mock, interval
-        const chainId = network.config.chainId
+          let raffle, VRFCoordinatorV2Mock, interval, raffleEntranceFee, deployer
+          const chainId = network.config.chainId
 
-        beforeEach(async function (){
-            const {deployer} = await getNamedAccounts()
-            await deployments.fixture(["all"])
-            raffle = await ethers.getContract("Raffle", deployer)
-            VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
-        })
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              await deployments.fixture(["all"])
+              raffle = await ethers.getContract("Raffle", deployer)
+              VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
+          })
 
-        describe("constructor", async function(){
-            it("initializes the raffle correctly", async function(){
-                // Ideally we make our test have just 1 assert per it()
-                const raffleState = await raffle.getRaffleState()
-                const interval = await raffle.getInterval()
-                assert.equal(raffleState.toString(), "0")
-                assert.equal(interval.toString(), networkConfig[chainId]["interval"])
-            })
-        })
-    })
+          describe("constructor", async function () {
+              it("initializes the raffle correctly", async function () {
+                  // Ideally we make our test have just 1 assert per it()
+                  const raffleState = await raffle.getRaffleState()
+                  const interval = await raffle.getInterval()
+                  assert.equal(raffleState.toString(), "0")
+                  assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+              })
+          })
+
+          describe("enterRaffle", async function () {
+              //  yarn hardhat test --grep "you don't pay enough"
+              it("reverts when you don't pay enough", async function () {
+                  await expect(raffle.enterRaffle()).to.be.revertedWith(
+                      "Raffle__NotEnoughETHEntered"
+                  )
+              })
+              // yarn hardhat test --grep "records players when they enter"
+              it("records players when they enter", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const playerFromContract = await raffle.getPlayer(0)
+                  assert.equal(playerFromContract, deployer)
+              })
+          })
+      })
