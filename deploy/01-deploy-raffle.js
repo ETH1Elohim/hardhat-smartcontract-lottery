1aab081969fc4051da4a249b56a608449aa5ad6a
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-12 17:37:29 -0700

    verification & helper configs

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,5 +1,6 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../helper-hardhat-config")
 
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
@@ -7,7 +8,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address, subscriptionId 
+    let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
         const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
@@ -35,4 +36,12 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, args)
+    }
+    log("--------------------------------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
