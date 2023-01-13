df3290afadf03e0f30adf3ddd8ced89be126aac8
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-12 18:07:42 -0700

    add verify

---
+++ utils/verify.js
@@ -0,0 +1,22 @@
+const { run } = require("hardhat")
+const { modules } = require("web3")
+
+// verify on etherscan
+// error with VPN on
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+module.exports = { verify }
